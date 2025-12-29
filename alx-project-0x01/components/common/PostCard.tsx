2b199268584ebf2c3192df7601e22959c0b411c0
kess4obehi <kessiena4excellence@gmail.com>
2025-12-29 22:48:18 +0100

    Implement Posts Card Components

--- alx-project-0x01/components/common/PostCard.tsx
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -1,10 +1,16 @@
-const PostCard: React.FC = () => {
+import { PostProps } from "@/interfaces";
+
+const PostCard: React.FC<PostProps> = ({ title, body, userId, id}) => {
   return (
-    <div className="border rounded-lg p-4 shadow-sm">
-      <h2 className="text-xl font-semibold">Post Title</h2>
-      <p className="text-gray-600 mt-2">
-        This is a sample post card component.
-      </p>
+    <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg">
+      <div className="mb-4">
+        <h2 className="text-2xl font-seminold text-gray-800">{title}</h2>
+      </div>
+      <p className="text-gray-600">{body}</p>
+      <div>
+        <span>User ID: {userId}</span>
+        <span>Post ID: {id}</span>
+      </div>
     </div>
   );
 };
