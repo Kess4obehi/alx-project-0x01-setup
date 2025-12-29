5171adea8e8d590b01d5aa81fb190af72a5f1af3
kess4obehi <kessiena4excellence@gmail.com>
2025-12-29 16:55:53 +0100

    Set up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/layout/Header";
+
 const PostPage: React.FC = () => {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-semibold">Posts Page</h1>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-2xl font-semibold">Posts Page</h1>
+      </main>
     </div>
   );
 };
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/layout/Header";
+
 const UsersPage: React.FC = () => {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-semibold">Users Page</h1>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-2xl font-semibold">Users Page</h1>
+      </main>
     </div>
   );
 };
